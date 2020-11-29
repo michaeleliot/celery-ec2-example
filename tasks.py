1282965b6d9946ea4e5999c6b98d0cf106d54d0e
Michael Eliot <michaeleliot@Michaels-MacBook-Pro.local>
2020-11-28 16:46:32 -0800

    added subtract

--- tasks.py
+++ tasks.py
@@ -5,3 +5,7 @@ app = Celery('tasks', broker='redis://localhost')
 @app.task
 def add(x, y):
     return x + y
+
+@app.task
+def subtract(x, y):
+    return x - y
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from pytest import raises
 
-from tasks import add
+from tasks import add, subtract
 
 def test_add():
     math = add(1, 2)
     assert math == 3
+
+def test_add():
+    math = subtract(2, 1)
+    assert math == 1
