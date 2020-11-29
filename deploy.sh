14803d965005a623aa62f19f433bac8bf52a2c48
Michael Eliot <michaeleliot@Michaels-MacBook-Pro.local>
2020-11-29 15:44:46 -0800

    switched back to deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,14 @@ curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-de
 sudo chmod +x /usr/bin/ecs-deploy
 
 # login AWS ECR
-eval $(aws ecr get-login --region us-east-1)
+eval $(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 752797697680.dkr.ecr.us-east-1.amazonaws.com)
 
 # or login DockerHub
 # docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PSW
 
 # build the docker image and push to an image repository
-docker build -t github-action-celery .
-docker tag github-action-celery:latest $IMAGE_REPO_URL:latest
+docker build -t test
+docker tag test:latest $IMAGE_REPO_URL:latest
 docker push $IMAGE_REPO_URL:latest
 
 # update an AWS ECS service with the new image
